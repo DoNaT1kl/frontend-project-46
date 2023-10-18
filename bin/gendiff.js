c5da9555f088c626036b3b7f1fd0b86097872288
Vlad <donatik919@gmail.com>
2023-10-18 17:28:26 +0300

    addArgumentsOption

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 const { Command } = require('commander');
-const program = new Command();
+const program = new Command('');
 
 program
   .version('0.1.0')
-  .description('Compare two configuration files and shows a difference.');
-
+  .description('Compare two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
 program.parse();
